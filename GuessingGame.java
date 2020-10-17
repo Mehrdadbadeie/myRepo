54b7025c2516ffd33d829926919117cc63e973ce
Mehrdad <mehbad2001@skola.goteborg.se>
2020-10-17 19:59:20 +0200

    changed high to 10 for faster testing

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,5 +1,5 @@
 public class GuessingGame {
     public static void main(String[] args) {
         
-        Guesser guess = new Guesser(0, 100);
+        Guesser guess = new Guesser(0, 10);
         guess.start();
