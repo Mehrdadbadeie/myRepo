366d36582ffd241792a1389b0daf177ceac147f2
Mehrdad <mehbad2001@skola.goteborg.se>
2020-10-18 16:26:08 +0200

    changed gamemode from 100 to 10

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,7 +1,7 @@
 public class GuessingGame {
     public static void main(String[] args) {
         
-        Guesser guess = new Guesser(0, 100);
+        Guesser guess = new Guesser(0, 10);
         guess.start();
         
     }
